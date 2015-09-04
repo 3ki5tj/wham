fa689b9e3a04cd6592fffdecff07060891e4b7b3
Cheng Zhang <3ki5tj@gmail.com>
2015-09-03 21:01:22 -0500

    move a few files

---
+++ data/1VII/mkTex.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+python prep.py -T 450 --nt=4
+python prep.py -T 500 --nt=4
+python prep.py -T 550 --nt=4
+python prep.py -T 600 --nt=4
+python prep.py -T 650 --nt=4
+python prep.py -T 700 --nt=4
+python prep.py -T 750 --nt=4
+python prep.py -T 800 --nt=4
+python prep.py -T 850 --nt=4
+python prep.py -T 900 --nt=4
+python prep.py -T 950 --nt=4
+python prep.py -T 1000 --nt=4
+
